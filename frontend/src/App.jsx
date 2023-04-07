e8f7f9c0ab660f5665384c90be96fff6c0dea83a
ItayFrisk <itay.manevych@gmail.com>
2023-04-07 23:13:30 +0300

    added the navbar componnent with dummy data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
-    <div className="App">
+    /*  <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -21,7 +22,8 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>  */
+    <Navbar />
   );
 }
 
