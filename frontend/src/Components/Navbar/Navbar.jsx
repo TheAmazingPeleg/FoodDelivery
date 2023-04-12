d860297465102156c33fab32aa56ddde0ecb4455
ItayFrisk <itay.manevych@gmail.com>
2023-04-12 19:32:36 +0300

    updated navbar.jsx and the navbar styling

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,32 +3,30 @@ import './Navbar.styling.css';
 
 function Navbar({ children }) {
   return (
-    <nav className="navbar">
-      <ol className="navbar-list">
+    <ol className="navbar-list">
 
-        <li className="navbar-left">
-          <a className="navbar-list-item-anchor" href="/">Home</a>
-          <a className="navbar-list-item-anchor" href="youtube.com">About Us</a>
+      <li className="navbar-left">
+        <a className="navbar-list-item-anchor" href="/">Home</a>
+        <a className="navbar-list-item-anchor" href="youtube.com">About Us</a>
+      </li>
+
+      <div className="navbar-middle">
+        <li className="navbar-list-item">
+          {React.Children.toArray(children).find((child) => child.type.name === 'SearchBar')}
         </li>
+      </div>
 
-        <div className="navbar-middle">
-          <li className="navbar-list-item">
-            {React.Children.toArray(children).find((child) => child.type.name === 'SearchBar')}
-          </li>
-        </div>
+      <li className="navbar-right">
+        {/* temporary */}
+        <button type="button" className="navbar-list-item-button">Login</button>
+        <button type="button" className="navbar-list-item-button">Sign Up</button>
+        <a href="example.com">
+          <img className="navbar-list-item-image" src="https://github.com/ColmanDevClub.png" alt="Colman Dev Club logo" />
+        </a>
+        {/* temporary */}
+      </li>
 
-        <li className="navbar-right">
-          {/* temporary */}
-          <button type="button" className="navbar-list-item-button">Login</button>
-          <button type="button" className="navbar-list-item-button">Sign Up</button>
-          <a href="example.com">
-            <img className="navbar-list-item-image" src="https://github.com/ColmanDevClub.png" alt="Colman Dev Club logo" />
-          </a>
-          {/* temporary */}
-        </li>
-
-      </ol>
-    </nav>
+    </ol>
   );
 }
 export default Navbar;
