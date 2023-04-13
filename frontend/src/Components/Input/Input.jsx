e24f06bdc23d8071182b6f47f952eb7ee8c76227
TheAmazingPeleg <peleg.rubin@gmail.com>
2023-04-13 20:28:29 +0300

    input eslint fix

--- frontend/src/Components/Input/Input.jsx
+++ frontend/src/Components/Input/Input.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react';
 import './Input.styling.css';
 
+function Input({ ...props }) {
+  const [value, setValue] = useState(props.value);
 
-const Input = (props) => {
-  const [value, setValue] = useState();
-
-  const handleChangeValue = (e) =>{
+  const handleChangeValue = (e) => {
     setValue(e.target.value);
-  }
+  };
   return (
     <div>
-      <lable>{props.name}:</lable>
+      <lable>
+        {props.name}
+        :
+      </lable>
       <input {...props} value={value} onChange={handleChangeValue} />
     </div>
   );
