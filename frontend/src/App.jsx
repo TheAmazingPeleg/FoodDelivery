af87ce02cbd8a6876a08224c596361823bfe4283
TheAmazingPeleg <peleg.rubin@gmail.com>
2023-04-17 22:54:38 +0300

    Adding Routes - Home, Cart, TrackOrder, Admin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-import Input from './Components/Input/Input';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar/Navbar';
+import Home from './pages/Home';
+import AdminHome from './pages/admin/Home';
+import Cart from './pages/Cart';
+import TrackOrder from './pages/TrackOrder';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <Input type="text" name="hey" value="yeey!!" />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/trackorder" element={<TrackOrder />} />
+          <Route path="/admin/dashboard" element={<AdminHome />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
