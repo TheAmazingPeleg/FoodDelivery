5993a7f128b54f03727f9c00fe5805f922615326
Nir Tuttnauer <21014429+nirtuttnauer@users.noreply.github.com>
2023-04-20 13:06:30 +0300

    cart update
    now its rendering!

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 
-function Cart(cartItems, setCartItems) {
+function Cart({ cartItems, setCartItems }) {
   const removeFromCart = (item) => {
     // eslint-disable-next-line react/destructuring-assignment
     const updatedCartItems = cartItems.filter((cartItem) => cartItem.id !== item.id);
     setCartItems(updatedCartItems);
   };
-
   return (
     <div>
       <h2>Cart</h2>
-      <ul>
-        {/* eslint-disable-next-line react/destructuring-assignment */}
-        {cartItems.map((item) => (
-          <li key={item.id}>
-            {item.name}
-            {' '}
-            -
-            {item.quantity}
-            {' '}
-            -
-            {item.price}
-            {/* eslint-disable-next-line react/button-has-type */}
-            <button onClick={() => removeFromCart(item)}>Remove</button>
-          </li>
-        ))}
-      </ul>
+      {cartItems.length === 0 ? (
+        <h2>Cart is empty, please add items to cart</h2>
+      ) : (
+        <ul>
+          {cartItems.map((item) => (
+            <li key={item.id}>
+              {item.name}
+              {' '}
+              -
+              {item.quantity}
+              {' '}
+              -
+              {item.price}
+              {/* eslint-disable-next-line react/button-has-type */}
+              <button onClick={() => removeFromCart(item)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
