95fbba65eba01e1a6c06818f7b2a86a021b5e803
TheAmazingPeleg <peleg.rubin@gmail.com>
2023-04-20 15:26:53 +0300

    Demo Object Array

--- frontend/src/pages/admin/Categories.jsx
+++ frontend/src/pages/admin/Categories.jsx
@@ -1,13 +1,14 @@
 import { React, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AdminAuthContext } from '../../Components/AuthContext/AdminAuthContext';
+import CategoriesList from '../../Components/Admin/CategoriesList';
 
 function Categories() {
   const AdminAuth = useContext(AdminAuthContext);
   const navigate = useNavigate();
   return (
     <div>
-      {AdminAuth ? 'Categories' : navigate('/')}
+      {AdminAuth ? <CategoriesList /> : navigate('/')}
     </div>
   );
 }
